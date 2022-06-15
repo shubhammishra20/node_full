4544acfb48a827175a1c18eefe759f4d8ab80de6
shubham <shubham.mishra@coradius.io>
2022-06-16 00:22:58 +0530

    my first code

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log("hello");
+// const chalk = require('chalk');
+// const http = require('http');
+
+// http.createServer((req, res) => {
+//     res.write("hi Iam shubham mishra")
+//     res.end()
+// }).listen(4500);
+// console.log(chalk.red("shubham mishra"));
+const chalk = require('chalk');
+console.log(chalk.red('Hello.... world!')); 
